961fe4f31492ab06eb976de51bc8d232a253c68f
zazplay <160972371+zazplay@users.noreply.github.com>
2024-11-11 19:51:05 +0200

    DeleteSvgAndConfigs

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
-  },
-  base: './',
-  server: {
-    port: 5173,
-    strictPort: true
-  }
 })
